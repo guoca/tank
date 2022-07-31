729b911799aca2de5e43f40fca9591c6b34a481a
guoca <995621809@qq.com>
2022-07-31 23:46:02 +0800

    调用链模式重构包名

--- src/com/mashibing/tank/chain/BulletWallCollider.java
+++ src/com/mashibing/tank/chain/BulletWallCollider.java
@@ -1,16 +1,18 @@
-package com.mashibing.tank.cor;
+package com.mashibing.tank.chain;
 
 import com.mashibing.tank.pojo.Bullet;
 import com.mashibing.tank.pojo.Wall;
+import com.mashibing.tank.pojo.base.BaseBullet;
 import com.mashibing.tank.pojo.base.BaseGameObject;
+import com.mashibing.tank.pojo.base.BaseWall;
 
 public class BulletWallCollider implements Collider {
 
     @Override
     public boolean collide(BaseGameObject o1, BaseGameObject o2) {
-        if (o1 instanceof Bullet && o2 instanceof Wall) {
-            Bullet b = (Bullet) o1;
-            Wall t = (Wall) o2;
+        if (o1 instanceof BaseBullet && o2 instanceof BaseWall) {
+            BaseBullet b = (BaseBullet) o1;
+            BaseWall t = (BaseWall) o2;
             if (b.getRect().intersects(t.getRect())) {
                 b.die();
             }
--- src/com/mashibing/tank/chain/Collider.java
+++ src/com/mashibing/tank/chain/Collider.java
@@ -1,4 +1,4 @@
-package com.mashibing.tank.cor;
+package com.mashibing.tank.chain;
 
 import com.mashibing.tank.pojo.base.BaseGameObject;
 
